30d11be008312683f30bcfb9171b40c4596bdf8e
sh3ki <shekaigarcia@gmail.com>
2026-03-10 22:03:10 +0800

    Create src/components/ui/section.tsx

---
+++ src/components/ui/section.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+
+type SectionProps = {
+  id?: string;
+  className?: string;
+  children: React.ReactNode;
+};
+
+export const Section = ({ id, className, children }: SectionProps) => {
+  return (
+    <section id={id} className={cn("py-20", className)}>
+      <div className="container-width">{children}</div>
+    </section>
+  );
+};
